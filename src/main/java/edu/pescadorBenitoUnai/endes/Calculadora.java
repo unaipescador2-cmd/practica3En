50e7d3c3f5153f602a5e7e2f2cc2ca1ea1376094
UNAIPESCADOR\unaip <pescadorbenito.unai@jgvaldemora.org>
2026-02-02 00:34:27 +0100

    Clase claculadora con las 4 operaciones y la division con IllegalArgumentException

--- src/main/java/edu/pescadorBenitoUnai/endes/Calculadora.java
+++ src/main/java/edu/pescadorBenitoUnai/endes/Calculadora.java
@@ -1,4 +1,48 @@
 package edu.pescadorBenitoUnai.endes;
+/**
+ * Esta clase impleentara las operaciones aritmeticas basicas
+ */
 
 public class Calculadora {
+    /**
+     * Suma de dos numeros reales
+     * @param a primer numero a sumar
+     * @param b segundo numero a sumar
+     * @return la suma de {@code a } y {@code b}
+     */
+    public double sumar (double a, double b){
+        return a+b;
+    }
+
+    /**
+     * resta de dos numeros reales
+     * @param a primer numero a restar
+     * @param b segundo nuemro a restar
+     * @return la resta de {@code a } y {@code b}
+     */
+    public double restar (double a, double b){
+        return a-b;
+    }
+    /**
+     * miltiplicacion de dos numeros reales
+     * @param a primer numero a multiplicar
+     * @param b segundo numero a multiplicar
+     * @return la multiplicacion de {@code a } y {@code b}
+     */
+    public double multiplicar (double a, double b){
+        return a*b;
+    }
+
+    /**
+     * division de dos numeros naturales
+     * @param a primer numero a dividir
+     * @param b segundo numero a dividir
+     * @return la division de {@code a } y {@code b}
+     */
+    public double dividir (double a, double b){
+        if (b==0){
+            throw new IllegalArgumentException("No se puede dividir entre 0");
+        }
+        return a/b;
+    }
 }
