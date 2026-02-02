120fd2bbb93ff3ba6590c4dacd0d1284bc13c622
UNAIPESCADOR\unaip <pescadorbenito.unai@jgvaldemora.org>
2026-02-02 22:12:20 +0100

    Clase conversor de temperatura completada

--- src/main/java/edu/pescadorBenitoUnai/endes/ConversorTemperatura.java
+++ src/main/java/edu/pescadorBenitoUnai/endes/ConversorTemperatura.java
@@ -1,4 +1,23 @@
 package edu.pescadorBenitoUnai.endes;
 
+/**
+ * Esta es la clase que nos permite convertir valores de temperatura de grados a celsius a fahrenheit
+ *
+ * <p>Usa las formulas matematicas estandar para realizar las conversiones</p>
+ */
 public class ConversorTemperatura {
+    /**
+     * convierte una temperatura de grados celsius a grados fahrenheit
+     *
+     * <p>Formula utilizada</p>
+     * <pre>
+     *     Fahrenheit = (Celsius * 9/5) + 32
+     * </pre>
+     *
+     * @param gahrenheit temperatura en grados Fahrenheit
+     * @return temperatura convertida a grados Celsius
+     */
+    public double fahrenheitACelsius(double fahrenheit){
+        return (fahrenheit-32)*5/9;
+    }
 }
